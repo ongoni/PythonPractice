cf111d629cfb7db545e2b856e8eaa8e74d42b780
ongoni <ongoni.fg@gmail.com>
2017-07-07 15:48:46 +0400

    methods became static

--- practice.py
+++ practice.py
@@ -10,7 +10,7 @@ from functools import reduce
 
 class MatrixFiller:
     @staticmethod
-    def fill_matrix_with_random(self, first_dimension: int, second_dimension: int,
+    def fill_matrix_with_random(first_dimension: int, second_dimension: int,
                                 start: int, stop: int):
         return numpy.array(numpy.random.random_integers(start, stop, (first_dimension, second_dimension)))
 
@@ -20,14 +20,16 @@ class Practice:
     title = 'Python Summer practice'
 
     class Part1:
-
-        def page21_part2_task1(self):
+        @staticmethod
+        def page21_part2_task1():
             print(math.sqrt(float(input('enter area: '))) * 4)
 
-        def page21_part3_task1(self):
+        @staticmethod
+        def page21_part3_task1():
             print(max(map(float, input('enter values: ').split())))
 
-        def page44_part1_task1(self):
+        @staticmethod
+        def page44_part1_task1():
             def check(x, y):
                 if y < 0 or x ** 2 + y ** 2 > 9:
                     print('no')
@@ -38,14 +40,17 @@ class Practice:
 
             check(float(input('enter x: ')), float(input('enter y: ')))
 
-        def page44_part3_task1(self):
+        @staticmethod
+        def page44_part3_task1():
             print(*range(1, 22, 2), sep = ' ')
 
-        def page44_part4_task1(self):
+        @staticmethod
+        def page44_part4_task1():
             for i in range(4):
                 print('5 ' * 6)
 
-        def page44_part5_task1(self):
+        @staticmethod
+        def page44_part5_task1():
             def f(x):
                 if x == -1:
                     print('function is not defined in x = -1')
@@ -59,7 +64,8 @@ class Practice:
                 f(a)
                 a += h
 
-        def page44_part6_task1(self):
+        @staticmethod
+        def page44_part6_task1():
             def f(x):
                 if x >= 0.9:
                     return 1.0 / (0.1 + x) ** 2
@@ -75,17 +81,20 @@ class Practice:
                 print(f(a))
                 a += h
 
-        def page59_part1_task1(self):
+        @staticmethod
+        def page59_part1_task1():
             n = int(input('enter n: '))
             print(reduce(lambda s, x: s + x ** 2, range(n + 1), 0))
 
-        def page59_part2_task1(self):
+        @staticmethod
+        def page59_part2_task1():
             k = int(input('enter k: '))
             x = float(input('enter x: '))
 
             print(reduce(lambda s, n: s + pow(x, n) / n, range(1, k + 1), 0))
 
-        def page59_part3_task1(self):
+        @staticmethod
+        def page59_part3_task1():
             def s(e):
                 s, i = 0, 1
                 while True:
@@ -110,23 +119,27 @@ class Practice:
             print('simple function: ', s(e))
             print('reduce: ', reduce_s(e))
 
-        def page88_part1_task1(self):
+        @staticmethod
+        def page88_part1_task1():
             a = list(int(e) for e in input('enter the array: ').split())
 
             print(*map(lambda x: -abs(x), a))
 
-        def page88_part2_task1(self):
+        @staticmethod
+        def page88_part2_task1():
             a = list(float(e) for e in input('enter the array: ').split())
             m = max(a)
 
             print(len(list(filter(lambda x: x == m, a))))
 
-        def page88_part5_task1(self):
+        @staticmethod
+        def page88_part5_task1():
             a = list(int(e) for e in input('enter the array: ').split())
 
             print(*filter(lambda x: x % 2 != 0, a))
 
-        def page30_task1(self):
+        @staticmethod
+        def page30_task1():
             def maximum(a, b):
                 if a >= b:
                     return a
@@ -141,7 +154,8 @@ class Practice:
 
             print((minimum(3 * x, 2 * y) + minimum(x - y, x + y)))
 
-        def page37_part1_task1(self):
+        @staticmethod
+        def page37_part1_task1():
             def is_prime(x):
                 if x % 2 == 0:
                     return x == 2
@@ -153,7 +167,8 @@ class Practice:
             a, b = map(int, input('enter a and b: ').split())
             print(*filter(is_prime, range(a, b + 1)))
 
-        def page37_part2_task1(self):
+        @staticmethod
+        def page37_part2_task1():
             def b(n):
                 if n == 1:
                     return -10
@@ -165,7 +180,8 @@ class Practice:
             n = int(input('enter n: '))
             print(b(n))
 
-        def page24_part2_task1(self):
+        @staticmethod
+        def page24_part2_task1():
             line = input('enter line: ')
             to_find = input('enter symbol to find: ')
             to_insert = input('enter symbol to insert: ')
@@ -180,7 +196,8 @@ class Practice:
 
             print(result)
 
-        def page4_part3_task1(self):
+        @staticmethod
+        def page4_part3_task1():
             regex = r'\b[A-Z]?[a-z]+\b|\b[A-Z]\b|\b[А-ЯЁ]\b|\b[А-ЯЁ]?[а-яё]+\b|\b[A-Z]+\b|\b[А-ЯЁ]+\b'
             text = re.findall(regex, input('enter text: '))
             word_to_find = input('enter word to find: ')
@@ -188,14 +205,15 @@ class Practice:
             print(len(list(filter(lambda x: x == word_to_find, text))))
 
     class Part2:
-
-        def part2_1_1_task1(self):
+        @staticmethod
+        def part2_1_1_task1():
             l = reduce(lambda a, x: a + [random.randrange(-50, 51)], range(20), [])
 
             print(*l)
             print(*filter(lambda x: x > 0 and x % 7 == 0, l))
 
-        def part2_1_2_task1(self):
+        @staticmethod
+        def part2_1_2_task1():
             l = reduce(lambda a, x: a + [random.randrange(-10, 130)], range(20), [])
 
             to_insert = int(input('enter x: '))
@@ -210,18 +228,21 @@ class Practice:
 
             print(*result)
 
-        def part2_1_3_task1(self):
+        @staticmethod
+        def part2_1_3_task1():
             l = reduce(lambda a, x: a + [random.randrange(-50, 51)], range(20), [])
             min_element = min(l)
 
             print(*filter(lambda x: x != min_element, l))
 
-        def part2_1_4_task1(self):
+        @staticmethod
+        def part2_1_4_task1():
             t = tuple(reduce(lambda a, x: a + [random.randrange(-50, 51) + random.random()], range(10), []))
 
             print(*sorted(list(t)))
 
-        def part2_2_5_task1(self):
+        @staticmethod
+        def part2_2_5_task1():
             t = tuple(reduce(lambda res, x: res + [random.randrange(-50, 51)], range(10), []))
             a, b = map(int, input('enter a and b: ').split())
 
@@ -236,7 +257,8 @@ class Practice:
             print(*t)
             print(*result)
 
-        def part2_2_6_task1(self):
+        @staticmethod
+        def part2_2_6_task1():
             a = set(reduce(lambda res, x: res + [random.randrange(-10, 10)], range(10), []))
             b = set(reduce(lambda res, x: res + [random.randrange(0, 11)], range(10), []))
 
@@ -245,13 +267,15 @@ class Practice:
             print(*(a & b))
             print(reduce(lambda res, x: res + x, a & b, 0))
 
-        def part2_3_task1(self):
+        @staticmethod
+        def part2_3_task1():
             l = list(input('enter list elements: ').split())
 
             d = dict(zip(l, range(len(l))))
             print(*d.items())
 
-        def part2_3_task2(self):
+        @staticmethod
+        def part2_3_task2():
             line = input('enter line: ').split()
             d = {'lol': 'kek', 'kek': 'lol'}
 
@@ -265,7 +289,8 @@ class Practice:
 
             print(result)
 
-        def part2_3_task3(self):
+        @staticmethod
+        def part2_3_task3():
             l = reduce(lambda a, x: a + [random.randrange(-50, 51)], range(20), [])
 
             first_max = max(l)
@@ -277,7 +302,8 @@ class Practice:
             print('second max - ' + str(second_max))
             print('third max - ' + str(third_max))
 
-        def part2_3_task4(self):
+        @staticmethod
+        def part2_3_task4():
             regex = r'\b[A-Z]?[a-z]+\b|\b[A-Z]\b|\b[А-ЯЁ]\b|\b[А-ЯЁ]?[а-яё]+\b|\b[A-Z]+\b|\b[А-ЯЁ]+\b'
             text = re.findall(regex, input('enter text: '))
             d = {}.fromkeys(text, 0)
@@ -290,37 +316,46 @@ class Practice:
     class Part3:
 
         class Block1:
-            def task1(self):
-                print(MatrixFiller.fill_matrix_with_random(self, 4, 4, -50, 50))
+            @staticmethod
+            def task1():
+                print(MatrixFiller.fill_matrix_with_random(4, 4, -50, 50))
 
-            def task2(self, matrix):
+            @staticmethod
+            def task2(matrix):
                 print('2. element with indexes [2, 3]: ', matrix[2, 3])
 
-            def task3(self, matrix):
+            @staticmethod
+            def task3(matrix):
                 print('3. first matrix line: ', matrix[0])
 
-            def task4(self, matrix):
+            @staticmethod
+            def task4(matrix):
                 print('4. every second element in 3rd line: ', matrix[2, ::2])
 
-            def task5(self, matrix, new_dimensions: tuple):
+            @staticmethod
+            def task5(matrix, new_dimensions: tuple):
                 new_matrix = matrix.reshape(new_dimensions[0], new_dimensions[1])
                 print('5. matrix with new dimensions: ', new_matrix)
                 return new_matrix
 
-            def task6(self, matrix):
+            @staticmethod
+            def task6(matrix):
                 print('6. matrix multipying by scalar: ')
                 scalar = int(input('enter scalar: '))
                 matrix = matrix.dot(scalar)
                 print(matrix)
                 return matrix
 
-            def task7(self, matrix):
+            @staticmethod
+            def task7(matrix):
                 print('7: minimum in every line: ', [min(matrix[i]) for i in range(matrix.shape[0])])
 
-            def task8(self, matrix):
+            @staticmethod
+            def task8(matrix):
                 print('8. maximum in last column: ', max(matrix[:, matrix.shape[1] - 1]))
 
-            def task9(self, matrix):
+            @staticmethod
+            def task9(matrix):
                 def get_elements_w_zero_before(vec):
                     prev_is_zero = False
                     result = []
@@ -337,7 +372,8 @@ class Practice:
                 v = numpy.array(matrix.reshape(1, matrix.shape[0] * matrix.shape[1]))[0].tolist()
                 print('9. maximum in elements which have 0 before it: ', max(get_elements_w_zero_before(v)))
 
-            def task10(self, matrix):
+            @staticmethod
+            def task10(matrix):
                 result = 1
                 for i in range(matrix.shape[0]):
                     result *= matrix[i, i]
@@ -345,10 +381,10 @@ class Practice:
                 print('10. result of multiplying elements from main giagonal: ', result)
 
         class Block2:
-
-            def task1(self):
+            @staticmethod
+            def task1():
                 n, m = map(int, input('enter n and m: ').split())
-                a = MatrixFiller.fill_matrix_with_random(self, n, m, -50, 100)
+                a = MatrixFiller.fill_matrix_with_random(n, m, -50, 100)
                 print(a)
 
                 maximum, index = a[0, 0], 0
@@ -359,9 +395,10 @@ class Practice:
                         index = i
                 print(max(a[:, index]))
 
-            def task6(self):
+            @staticmethod
+            def task6():
                 n, m = map(int, input('enter n and m: ').split())
-                a = MatrixFiller.fill_matrix_with_random(self, n, m, -50, 100)
+                a = MatrixFiller.fill_matrix_with_random(n, m, -50, 100)
                 print(a, end='\n\n')
 
                 sum_of_columns, sum_of_all = sum(a), sum(sum(a))
@@ -370,9 +407,10 @@ class Practice:
 
                 print(a)
 
-            def task11(self):
+            @staticmethod
+            def task11():
                 n, m, l = map(int, input('enter n, m and l: ').split())
-                a = MatrixFiller.fill_matrix_with_random(self, n, m, -50, 100)
+                a = MatrixFiller.fill_matrix_with_random(n, m, -50, 100)
                 print(a)
 
                 for i in range(n):
@@ -380,18 +418,20 @@ class Practice:
 
                 print(a)
 
-            def task16(self):
+            @staticmethod
+            def task16():
                 n, m = map(int, input('enter n and m: ').split())
-                a = MatrixFiller.fill_matrix_with_random(self, n, m, -50, 100)
+                a = MatrixFiller.fill_matrix_with_random(n, m, -50, 100)
                 print(a, end='\n\n')
 
                 l = int(input('enter l: '))
 
                 print(numpy.delete(a, l, axis=0))
 
-            def task21(self):
+            @staticmethod
+            def task21():
                 n, m = map(int, input('enter n and m: ').split())
-                a = MatrixFiller.fill_matrix_with_random(self, n, m, -50, 100)
+                a = MatrixFiller.fill_matrix_with_random(n, m, -50, 100)
                 print(a)
 
                 min_dim = min((n, m))
@@ -401,9 +441,10 @@ class Practice:
 
                 print(a)
 
-            def task26(self):
+            @staticmethod
+            def task26():
                 n, m = map(int, input('enter n and m: ').split())
-                a = MatrixFiller.fill_matrix_with_random(self, n, m, -50, 100)
+                a = MatrixFiller.fill_matrix_with_random(n, m, -50, 100)
                 print(a)
 
                 l, k = map(int, input('enter l and k: ').split())
@@ -433,8 +474,8 @@ class Practice:
                       (fourth.shape[0] * fourth.shape[1]))
 
     class Part4:
-
-        def block1(self):
+        @staticmethod
+        def block1():
             df = pandas.read_csv('titanic.csv')
             print(df)
 
@@ -459,7 +500,8 @@ class Practice:
             print(round(len(children) / (len(df) / 100), 2),
                   '% of passengers were children')
 
-        def block2(self):
+        @staticmethod
+        def block2():
             def f(x):
                 if x >= 0.9:
                     return 1.0 / (0.1 + x) ** 2
@@ -490,8 +532,8 @@ class Practice:
             print('table loaded from created file:\n', pandas.read_csv('my_own_table.csv', index_col=0))
 
     class Part5:
-
-        def task1(self):
+        @staticmethod
+        def task1():
             def f(x):
                 if x >= 0.9:
                     return 1.0 / (0.1 + x) ** 2
@@ -505,5 +547,3 @@ class Practice:
             y_values = [f(x) for x in x_values]
             pylab.plot(x_values, y_values)
             pylab.show()
-
-Practice.Part4.block1(Practice.Part4)
